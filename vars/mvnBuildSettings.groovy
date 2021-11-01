6ea156f197e472c4f46b06b69df2c94a82f00e53
Reda <reda@example.com>
2021-11-01 18:34:27 +0100

    modify setting.xml

---
+++ vars/mvnBuildSettings.groovy
@@ -0,0 +1,33 @@
+import com.pwc.dfy.services.DevOpsConf
+
+/**
+ * build project with settings.xml
+ * @param parameters
+ * @return
+ */
+def call(Map parameters = [:]) {
+
+    def param = parameters.get('param')
+
+    try {
+
+                //Settings-security
+//                def filesecurityPath = DevOpsConf.DFY_SETTINGS_SECURITY_FILE
+//                def contentSecurityFile = libraryResource filesecurityPath
+//                def pathSecurityManifest = './home/jenkins/.m2/' + 'settings-security.xml'
+//                writeFile file: pathSecurityManifest, text: contentSecurityFile
+
+                //settings.xml
+                def filePath = DevOpsConf.DFY_SETTINGS_FILE
+                def contentfile = libraryResource filePath
+                def pathManifest = './home/jenkins/.m2/' + 'settings.xml'
+                writeFile file: pathManifest, text: contentfile
+
+                sh "mvn ${param} -s ${pathManifest}"
+
+
+    } catch (err) {
+        currentBuild.result = 'FAILED'
+        throw err
+    }
+}
