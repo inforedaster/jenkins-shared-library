39ed14c003f501224f2baa416d6ba49a1505e91f
Reda <reda@example.com>
2021-11-01 20:15:01 +0100

    modify setting.xml

--- vars/deploytonexus.groovy
+++ vars/deploytonexus.groovy
@@ -8,8 +8,7 @@ import com.pwc.dfy.services.Utilities
 def call(Map parameters = [:]) {
 
     def utilities = new Utilities()
-    sh "curl http://www.java2s.com/Code/JarDownload/java-/java-1.0.jar.zip --output myfile.zip"
-    utilities.deployArtifactToNexusWithPackaging("com.file.nexus", tok, "1.0.${BUILD_NUMBER}", "jar", "thirdparty", "http://localhost:8081/repository/maven-snapshots/", "myfile.jar")
+    utilities.deployArtifactToNexusWithPackaging("com.file.nexus", tok, "1.0.${BUILD_NUMBER}", "zip", "thirdparty", "http://localhost:8081/repository/maven-snapshots/", "myfile.zip")
 
 
 }
