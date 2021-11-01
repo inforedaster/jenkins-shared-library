f01aeed04fd0eefa5f0c4f30d1902b092a4fdea0
Reda <reda@example.com>
2021-11-01 21:01:37 +0100

    modify setting.xml

--- vars/deploytonexus.groovy
+++ vars/deploytonexus.groovy
@@ -8,7 +8,18 @@ import com.pwc.dfy.services.Utilities
 def call(Map parameters = [:]) {
 
     def utilities = new Utilities()
-    utilities.deployArtifactToNexusWithPackaging("com.file.nexus", "tok", "1.0.${BUILD_NUMBER}", "zip", "thirdparty", "http://172.17.0.3:8081/content/repositories/maven-snapshots/", "myfile.zip")
+    def mvn = tool(DevOpsConf.MAVEN_INSTALLATION)
+    sh """
+           ${mvn}/bin/mvn deploy:deploy-file \
+          -DgroupId="com.file.nexus" \
+          -DartifactId="jenkins" \
+          -Dversion="1.0.${BUILD_NUMBER}" \
+          -DgeneratePom=false \
+          -DrepositoryId="maven-snapshots" \
+          -Durl="http://172.17.0.3:8081/content/repositories/maven-snapshots/" \
+          -Dfile="myfile.zip"
+      """
+    //utilities.deployArtifactToNexusWithPackaging("com.file.nexus", "tok", "1.0.${BUILD_NUMBER}", "zip", "thirdparty", "http://172.17.0.3:8081/content/repositories/maven-snapshots/", "myfile.zip")
 
 
 }
