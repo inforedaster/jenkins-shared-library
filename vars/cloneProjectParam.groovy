350497031f83bb80aaf43874fadfb922ae24fd22
Reda <reda@example.com>
2021-11-01 09:42:49 +0100

    modif

--- vars/cloneProjectParam.groovy
+++ vars/cloneProjectParam.groovy
@@ -1,3 +1,5 @@
+import com.pwc.dfy.services.DevOpsConf
+
 /**
  * clone git project with git url in parameters
  * @param gitUrl : the git link to clone into the job(this param must exist in the pipeline script)
@@ -5,10 +7,12 @@
  */
 def call(Map parameters = [:]) {
 
-    def gitUrl = parameters.get('gitUrl')
+    //def gitUrl = parameters.get('gitUrl')
+    def git = tool("git")
 
         //$branch pram is a job param
 
-            git branch: "$branch", url: "$gitUrl"
+            //git branch: "$branch", url: "$gitUrl"
+    sh "${git} clone https://github.com/inforedaster/jenkins-shared-library.git "
 
 }
