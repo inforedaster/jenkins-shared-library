2987a8bf4e3ef9b7e7aa161d4815e46eb3a65756
Reda <reda@example.com>
2021-11-04 21:39:02 +0100

    add nexus variable

--- vars/deploytonexus.groovy
+++ vars/deploytonexus.groovy
@@ -10,7 +10,9 @@ def call(Map parameters = [:]) {
     def utilities = new Utilities()
     def mvn = tool(DevOpsConf.MAVEN_INSTALLATION)
     def nexus = parameters.get('nexus')
-    sh """
+
+    sh "${mvn}/bin/mvn clean deploy  -Dnexus=${nexus}"
+    /*sh """
            ${mvn}/bin/mvn deploy:deploy-file \
           -DgroupId="com.file.nexus" \
           -DartifactId="jenkins" \
@@ -19,7 +21,7 @@ def call(Map parameters = [:]) {
           -DrepositoryId="maven-snapshots" \
           -Durl="${nexus}/content/repositories/maven-snapshots/" \
           -Dfile="myfile.zip"
-      """
+      """*/
     //utilities.deployArtifactToNexusWithPackaging("com.file.nexus", "tok", "1.0.${BUILD_NUMBER}", "zip", "thirdparty", "http://172.17.0.3:8081/content/repositories/maven-snapshots/", "myfile.zip")
 
 
--- vars/mvnBuildSettings.groovy
+++ vars/mvnBuildSettings.groovy
@@ -13,11 +13,6 @@ def call(Map parameters = [:]) {
 
     try {
 
-                //Settings-security
-//                def filesecurityPath = DevOpsConf.DFY_SETTINGS_SECURITY_FILE
-//                def contentSecurityFile = libraryResource filesecurityPath
-//                def pathSecurityManifest = './home/jenkins/.m2/' + 'settings-security.xml'
-//                writeFile file: pathSecurityManifest, text: contentSecurityFile
 
                 //settings.xml
                 def filePath = DevOpsConf.ITOPS_SETTINGS_FILE
@@ -25,8 +20,6 @@ def call(Map parameters = [:]) {
                 def pathManifest = './home/jenkins/.m2/' + 'settings.xml'
                 writeFile file: pathManifest, text: contentfile
 
-               // sh "${mvn}/bin/mvn clean ${param} -s ${pathManifest}"
-                //sh "${mvn}/bin/mvn clean install -s ${pathManifest}"
 
         sh """
            ${mvn}/bin/mvn deploy:deploy-file \
