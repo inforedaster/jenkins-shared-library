cf192d07bb7ec6f2ac514c3d65602cbf642e91bf
Reda <reda@example.com>
2021-11-04 21:32:25 +0100

    add nexus variable

--- vars/deploytonexus.groovy
+++ vars/deploytonexus.groovy
@@ -9,6 +9,7 @@ def call(Map parameters = [:]) {
 
     def utilities = new Utilities()
     def mvn = tool(DevOpsConf.MAVEN_INSTALLATION)
+    def nexus = parameters.get('nexus')
     sh """
            ${mvn}/bin/mvn deploy:deploy-file \
           -DgroupId="com.file.nexus" \
@@ -16,7 +17,7 @@ def call(Map parameters = [:]) {
           -Dversion="1.0.${BUILD_NUMBER}" \
           -DgeneratePom=false \
           -DrepositoryId="maven-snapshots" \
-          -Durl="http://172.17.0.3:8081/content/repositories/maven-snapshots/" \
+          -Durl="${nexus}/content/repositories/maven-snapshots/" \
           -Dfile="myfile.zip"
       """
     //utilities.deployArtifactToNexusWithPackaging("com.file.nexus", "tok", "1.0.${BUILD_NUMBER}", "zip", "thirdparty", "http://172.17.0.3:8081/content/repositories/maven-snapshots/", "myfile.zip")
